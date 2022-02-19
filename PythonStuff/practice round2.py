f3381713e4ab231b28238575c178fdf0e170ed7e
Jackie Cai <jackiecai2006@outlook.com>
2022-02-18 18:18:38 -0700

    Update practice round2.py

--- PythonStuff/practice round2.py
+++ PythonStuff/practice round2.py
@@ -8,16 +8,25 @@ def solve():
     length = int(input())
     papers = list(map(int, input().split()))
     final = []
+    greatest_list = []
     H = 0
     for i in range(length):
-        papers[i]
+        if papers[i] > H:
+            greatest_list.append(papers[i])
+        H = len(greatest_list)
+        for num in greatest_list:
+            if num < H:
+                greatest_list.remove(num)
+        H = len(greatest_list)
+        final.append(H)
+    return final
 
 
 if __name__ == '__main__':
     T = int(input())
     for i in range(T):
-        print(f'Case #{i+1}: ')
-        print(' '.join(solve()))
+        print(f'Case #{i+1}:', end=' ')
+        print(' '.join(list(map(str, solve()))))
 
 
 
